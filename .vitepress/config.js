93e1b37b630952637d60d0219e3bc1df72d3179f
Oliver <2799274183@qq.com>
2025-02-19 10:21:41 +0800

    feat: gaia client init

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,31 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  title: 'raina-gaia',  
-  base: '/gaia-release',
-  description: '大规模图形渲染工具', 
-  srcDir: './docs',
+  title: "raina-gaia",
+  base: "/gaia-release",
+  description: "大规模图形渲染工具",
+  srcDir: "./docs",
   themeConfig: {
     nav: [
-        { text: 'Overview', link: '/' },
-        { text: 'Server Docs', link: '/server/'},
-        { text: 'Client Docs', link: '/client/' },
+      { text: "Overview", link: "/" },
+      { text: "Server Docs", link: "/server/" },
+      { text: "Client Docs", link: "/client/" },
     ],
     sidebar: {
-        '/': [{ text: 'Overview', link: '/'}],
-        '/server/': [
-            {text: 'Quick Start', link: '/server/quick_start'},
-            {text: 'API Reference', link: '/server/api_reference'},
-            {text: 'Other Notes', link: '/server/other_notes'},
-            {text: 'About', link: '/server/about'},   
-        ],
-        '/client/':[
-
-        ]
+      "/": [{ text: "Overview", link: "/" }],
+      "/server/": [
+        { text: "Quick Start", link: "/server/quick_start" },
+        { text: "API Reference", link: "/server/api_reference" },
+        { text: "Other Notes", link: "/server/other_notes" },
+        { text: "About", link: "/server/about" },
+      ],
+      "/client/": [],
     },
     footer: {
-      message: 'raina-gaia release',
+      message: "raina-gaia release",
     },
   },
-
 });
